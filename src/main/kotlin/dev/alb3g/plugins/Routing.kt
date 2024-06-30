a3a7ead4b8daa0c845087a95ea4da4a49426b88c
Alberto <guzmanmorenoalberto1998@gmail.com>
2024-06-30 12:02:18 +0200

    Added all endpoints to the server

--- src/main/kotlin/dev/alb3g/plugins/Routing.kt
+++ src/main/kotlin/dev/alb3g/plugins/Routing.kt
@@ -1,7 +1,10 @@
 package dev.alb3g.plugins
 
+import dev.alb3g.models.Note
 import dev.alb3g.repository.NotesRepository
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
@@ -10,6 +13,60 @@ fun Application.configureRouting() {
         route("/notes") {
             get {
                 call.respond(NotesRepository.getAll())
+            }
+            // SELECT
+            get("{id}") {
+                val noteId = call.parameters["id"] ?: return@get call.respond(
+                    HttpStatusCode.BadRequest,
+                    "Missing or malformed ID"
+                )
+
+                val note = NotesRepository.getById(noteId.toLong()) ?: return@get call.respond(
+                    HttpStatusCode.NotFound,
+                    "No note found with Id: $noteId"
+                )
+
+                call.respond(note)
+            }
+            // CREATE
+            post {
+                try {
+                    val note = call.receive<Note>()
+                    val savedNote = NotesRepository.save(note)
+                    call.respond(HttpStatusCode.Created, savedNote)
+                }catch (e: Exception) {
+                    call.respond(HttpStatusCode.BadRequest, "Bad JSON Data Body: ${e.message}")
+                }
+            }
+            // UPDATE
+            put {
+                try {
+                    val note = call.receive<Note>()
+                    if(NotesRepository.update(note)) {
+                        call.respond(HttpStatusCode.OK, note)
+                    } else {
+                        call.respond(HttpStatusCode.NotFound, "No note found with Id: ${note.id}")
+                    }
+                }catch (e: Exception) {
+                    call.respond(HttpStatusCode.BadRequest, "Bad JSON Data Body: ${e.message}")
+                }
+            }
+            // DELETE
+            delete("{id}") {
+                val noteId = call.parameters["id"] ?: return@delete call.respond(
+                    HttpStatusCode.BadRequest,
+                    "Missing or malformed ID"
+                )
+                val note = NotesRepository.getById(noteId.toLong()) ?: return@delete call.respond(
+                    HttpStatusCode.NotFound,
+                    "No note found with Id: $noteId"
+                )
+                val deleteOperationResult = NotesRepository.delete(note.id);
+
+                when(deleteOperationResult) {
+                    true -> call.respond(HttpStatusCode.OK, note)
+                    else -> call.respond(HttpStatusCode.NotFound)
+                }
             }
         }
     }
--- src/main/kotlin/dev/alb3g/repository/NotesRepository.kt
+++ src/main/kotlin/dev/alb3g/repository/NotesRepository.kt
@@ -3,15 +3,21 @@ package dev.alb3g.repository
 import dev.alb3g.models.Note
 
 object NotesRepository {
-    fun getAll(): List<Note> {
-        val notes = (1..10).map {
-            Note(
-                it,
-                "Title $it" ,
-                "Description $it",
-                if(it % 2 == 0) Note.Type.SOUND else Note.Type.TEXT
-            )
-        }
-        return notes;
-    }
+
+    private val notes = mutableListOf<Note>()
+    private var currentId = 1L
+
+    fun save(note: Note): Note = note.copy(id = currentId++).also { notes.add(it) }
+
+    fun getAll() = notes;
+
+    fun getById(id: Long): Note? = notes.find { it.id == id } // find devuelve un T?
+
+    fun update(note: Note): Boolean  = notes
+        .indexOfFirst { it.id == note.id }
+        .takeIf { it >= 0 }
+        ?.also { notes[it] = note }
+        .let { it != null }
+
+    fun delete(id: Long): Boolean = notes.removeIf { note -> note.id == id }
 }
